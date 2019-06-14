5e7c5d80c8182f2a4efa86205194630051fc4a0e
Raher Camille <raher.camille@gmail.com>
2019-06-14 12:27:23 +0200

    UPDATE : Transition screen cosmos earth

---
+++ assets/js/loaderclick.js
@@ -0,0 +1,23 @@
+$( "#btnNext" ).on( "click", function() {
+    console.log('CLICK CLICK CLICK');
+    $(".loader").css('display', 'block').animate({
+        opacity: 1,
+    }, 1000);
+
+    setTimeout(
+        function(){ window.location =  './cosmos.html' }, 3000
+    );
+
+});
+
+$( "#cosmosNextScreen" ).on( "click", function() {
+    console.log('CLICK CLICK CLICK');
+    $(".loader").css('display', 'block').animate({
+        opacity: 1,
+    }, 1000);
+
+    setTimeout(
+        function(){ window.location =  './earth.html' }, 3000
+    );
+
+});
