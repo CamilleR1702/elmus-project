d8be039101094a05399b7928f58734100d877de6
melissacht <melissa.chanteux@gmail.com>
2019-06-14 12:08:01 +0200

    reglage ecran ocean

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,15 +24,57 @@ setInterval(function(){
 
 
 // SWIM
-setInterval(function(){
-  $('#elmusSwim').addClass('active');
-  setTimeout(function(){
-    $('#elmusSwim').addClass('active-swim');
-  }, 1000);
-}, 1);
+setTimeout(function(){
+  $('#elmusSwim').addClass('active-swim');
+}, 1000);
+
+// STOP SWIM
+setTimeout(function(){
+  $('#elmusSwim').removeClass('active-swim');
+  $('#elmusSwim').addClass('stop-swim');
+}, 24000);
+
+// DIRECTION VOLCANO
+setTimeout(function(){
+  $('#elmusSwim').addClass('active-swim-volcano');
+}, 25000);
+
+setTimeout(function(){
+  $('#elmusSwim').removeClass('active-swim-volcano');
+  $('#elmusSwim').addClass('stop-swim-volcano');
+}, 26000);
+
+// ELMUS SIDE
+setTimeout(function(){
+  $('#elmusSide').addClass('active');
+  $('#elmusSwim').addClass('none');
+}, 27000);
 
 //BACKGROUND ANIMATION OCEAN
 setInterval(function(){
   $('#ocean').addClass('active');
   $('#oceanBlack').addClass('active');
 }, 1);
+
+// VOLCANO
+setInterval(function(){
+  $('#bubble').addClass('active');
+  $('#seeds').removeClass('active');
+}, 30000);
+
+
+// BACTERIUM
+setInterval(function(){
+  $('#bubble').removeClass('active');
+  $('#bacterium').addClass('active');
+}, 35000);
+
+setInterval(function(){
+  $('#bacterium').addClass('infinite');
+}, 36000);
+
+
+// SEED
+setInterval(function(){
+  $('#seeds').addClass('active');
+}, 28000);
